5d365c829ffa3181c553025337a67fa5800c7792
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-20 23:58:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,15 @@
 def new_hash
   # return an empty hash
+  {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-end
+  my_hash  {
+  :created => Time.now,
+  :message => "Hello world!"
+}
+end 
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
